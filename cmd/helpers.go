ee2e5d502a1fd9f723dd2f97aab3fe7c06159ec2
Артемий Беляков (Москва) <a.belyakov@cloud.gcm>
2025-06-10 17:49:36 +0300

    Добавить в конфиг настройки профилирования 	modified:   README.md 	modified:   cmd/helpers.go 	modified:   cmd/server.go 	modified:   config/config.yml 	modified:   internal/confighandler/handler.go 	modified:   internal/confighandler/types.go 	modified:   test/confighandler/config_test.go 	modified:   version

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -9,24 +9,34 @@ import (
 	"github.com/av-belyakov/placeholder_doc-base_db/constants"
 	"github.com/av-belyakov/placeholder_doc-base_db/internal/appname"
 	"github.com/av-belyakov/placeholder_doc-base_db/internal/appversion"
+	"github.com/av-belyakov/placeholder_doc-base_db/internal/confighandler"
 )
 
-func getInformationMessage() string {
+func getInformationMessage(conf *confighandler.ConfigApp) string {
 	version, err := appversion.GetAppVersion()
 	if err != nil {
 		log.Println(err)
 	}
 
+	var profiling string
 	appStatus := fmt.Sprintf("%vproduction%v", constants.Ansi_Bright_Blue, constants.Ansi_Reset)
 	envValue, ok := os.LookupEnv("GO_PHDOCBASEDB_MAIN")
 	if ok && (envValue == "development" || envValue == "test") {
 		appStatus = fmt.Sprintf("%v%s%v", constants.Ansi_Bright_Red, envValue, constants.Ansi_Reset)
+
+		host := "*"
+		port := conf.Common.Profiling.Port
+
+		if port > 0 {
+			profiling = fmt.Sprintf("%vProfiling: %v'%s:%d/debug/pprof'%v\n", constants.Ansi_Bright_Green, constants.Ansi_Bright_Blue, host, port, constants.Ansi_Reset)
+		}
 	}
 
 	msg := fmt.Sprintf("Application '%s' v%s was successfully launched", appname.GetAppName(), strings.Replace(version, "\n", "", -1))
 
 	fmt.Printf("\n%v%v%s.%v\n", constants.Bold_Font, constants.Ansi_Bright_Green, msg, constants.Ansi_Reset)
 	fmt.Printf("%v%vApplication status is '%s'.%v\n", constants.Underlining, constants.Ansi_Bright_Green, appStatus, constants.Ansi_Reset)
+	fmt.Println(profiling)
 
 	return msg
 }
